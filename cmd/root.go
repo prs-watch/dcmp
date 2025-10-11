6d8bcc62816205f334d6b7bb99c1034a0864a53a
hctaw-srp <scarsdaleny15@yahoo.co.jp>
2025-10-11 14:07:52 +0000

    ~fix command descriptions

--- cmd/root.go
+++ cmd/root.go
@@ -12,11 +12,10 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use: "dcmp",
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
-	Args: cobra.ExactArgs(2),
+	Use:     "dcmp [path] [path]",
+	Short:   "Compare files you pass to dcmp command.",
+	Example: "dcmp hoge.txt fuga.txt",
+	Args:    cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		err := internal.Execute(args[0], args[1])
 		if err != nil {
@@ -36,13 +35,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.dcmp.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// nope
 }
